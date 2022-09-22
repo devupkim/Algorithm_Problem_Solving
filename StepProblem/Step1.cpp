c61cc4b224a14a96b6c69efe66ea75cc0e5c7cb3
Taehyun Kim <devupkim@gmail.com>
2022-09-22 17:33:39 +0900

    Solved Backjoon Step Problem 1-2

--- StepProblem/Step1.cpp
+++ StepProblem/Step1.cpp
@@ -2,7 +2,13 @@
 // Step1: 입출력과 사칙연산
 
 // Step1-1
-include <iostream>
+#include <iostream>
 int main() {
     std::cout<<"Hello World!";
+}
+
+// Step1-2
+#include<iostream>
+int main(){
+    std::cout<<"강한친구 대한육군\n강한친구 대한육군";
 }
