c57e21551c126c54f49b63248df205503440f085
TaeHyun Kim <devupkim@gmail.com>
2022-09-24 23:51:43 +0900

    #1000 Solved Backjoon Step Problem 1-3
    https://www.acmicpc.net/problem/1000

--- StepProblem/Step1.cpp
+++ StepProblem/Step1.cpp
@@ -2,13 +2,24 @@
 // Step1: 입출력과 사칙연산
 
 // Step1-1
+//https://www.acmicpc.net/problem/2557
 #include <iostream>
 int main() {
     std::cout<<"Hello World!";
 }
 
 // Step1-2
+//https://www.acmicpc.net/problem/10718
 #include<iostream>
 int main(){
     std::cout<<"강한친구 대한육군\n강한친구 대한육군";
+}
+
+// Step 1-3
+//https://www.acmicpc.net/problem/1000
+#include<iostream>
+int main(){
+    int a,b;
+    std::cin>>a>>b;
+    std::cout<<a+b;
 }
