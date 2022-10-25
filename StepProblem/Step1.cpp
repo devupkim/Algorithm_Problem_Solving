be6f0217c5548b74558e3a2c15f321b4298cd3e3
Taehyun Kim <devupkim@gmail.com>
2022-10-26 06:14:08 +0900

    #10926 Solved Backjoon Step Problem 1-7
    Step1-2 problem number 10718 has deleted from the step 1-2, So The others have -1 Step
    https://www.acmicpc.net/problem/10718

--- StepProblem/Step1.cpp
+++ StepProblem/Step1.cpp
@@ -8,14 +8,14 @@ int main() {
     std::cout<<"Hello World!";
 }
 
-// Step1-2
+// Step1-2 //Erased
 //https://www.acmicpc.net/problem/10718
 #include<iostream>
 int main(){
     std::cout<<"강한친구 대한육군\n강한친구 대한육군";
 }
 
-// Step 1-3
+// Step 1-2
 //https://www.acmicpc.net/problem/1000
 #include<iostream>
 int main(){
@@ -24,7 +24,7 @@ int main(){
     std::cout<<a+b;
 }
 
-// Step 1-4
+// Step 1-3
 //https://www.acmicpc.net/problem/1001
 #include<iostream>
 int main(){
@@ -33,7 +33,7 @@ int main(){
     std::cout<<a-b;
 }
 
-// Step 1-5
+// Step 1-4
 // https://www.acmicpc.net/problem/10998
 #include<iostream>
 int main(){
@@ -42,7 +42,7 @@ int main(){
     std::cout<<a*b;
 }
 
-// Step 1-6
+// Step 1-5
 // https://www.acmicpc.net/problem/1008
 #include<iostream>
 int main(){
@@ -53,7 +53,7 @@ int main(){
     std::cout<<a/b;
 }
 
-// Step 1-7
+// Step 1-6
 // https://www.acmicpc.net/problem/10869
 #include <iostream>
 
@@ -65,5 +65,17 @@ int main(){
     std::cout<<a*b<<std::endl;
     std::cout<<a/b<<std::endl;
     std::cout<<a%b<<std::endl;
+    return 0;
+}
+
+// Step 1-7
+// https://www.acmicpc.net/problem/10926
+#include <iostream>
+#include <string>
+
+int main(){
+    std::string str;
+    std::cin>>str;
+    std::cout<<str<<"??!";
     return 0;
 }
