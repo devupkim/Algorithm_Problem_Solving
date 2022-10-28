168821d179f0f9106a4fb121561750de027723bb
Taehyun Kim <devupkim@gmail.com>
2022-10-29 03:52:33 +0900

    #1330 Solved Backjoon Step Problem 2-1

---
+++ StepProblem/Step2.cpp
@@ -0,0 +1,38 @@
+// Backjoon Step Problem Solving
+// Step2 조건문
+
+// Step2-1
+// https://www.acmicpc.net/problem/1330
+#include<iostream>
+int main(){
+        int a,b;
+        std::cin>>a>>b;
+        if(a>b){
+                std::cout<<">";
+        }
+        else if(a<b){
+                std::cout<<"<";
+        }
+        else{
+                std::cout<<"==";
+        }
+        return 0;
+}
+
+// Step2-2
+
+
+// Step2-3
+
+
+// Step2-4
+
+
+// Step2-5
+
+
+// Step2-6
+
+
+// Step2-7
+
