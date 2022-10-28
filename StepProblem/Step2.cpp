5b5a851b078b6046985d43467ddc0b66a294947d
Taehyun Kim <devupkim@gmail.com>
2022-10-29 03:59:23 +0900

    #9498 Solved Backjoon Step Problem 2-2

--- StepProblem/Step2.cpp
+++ StepProblem/Step2.cpp
@@ -20,7 +20,18 @@ int main(){
 }
 
 // Step2-2
-
+// https://www.acmicpc.net/problem/9498 
+#include<iostream>
+int main(){
+        int a;
+        std::cin>>a;
+        if(a>=90){                std::cout<<"A";        }
+        else if(a>=80){                std::cout<<"B";        }
+        else if(a>=70){                std::cout<<"C";        }
+        else if(a>=60){                std::cout<<"D";        }
+        else{                std::cout<<"F";        }
+        return 0;
+}
 
 // Step2-3
 
