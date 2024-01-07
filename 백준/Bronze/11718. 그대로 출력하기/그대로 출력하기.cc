232a765d6a1f8b27361aee63723600e1104b7e2c
Taehyun Kim <devupkim@gmail.com>
2024-01-07 23:56:31 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define fastio cin.tie(0)->sync_with_stdio(0)
+using namespace std;
+
+
+int main(){
+    string s;
+    while(true){
+        getline(cin, s);
+        if(s=="") break;
+        
+        cout << s << '\n';
+    }
+    return 0;
+}
