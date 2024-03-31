022ecfd1fcdcb6f6dd7a719d179abe139b65c77b
Taehyun Kim <devupkim@gmail.com>
2024-03-31 23:51:00 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,17 @@
+#pragma GCC optimize("O3")
+
+#include <bits/stdc++.h>
+
+#define endl "\n"
+#define ll long long
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    char c;
+    cin >> c;
+    cout << (int) c;
+    return 0;
+}
