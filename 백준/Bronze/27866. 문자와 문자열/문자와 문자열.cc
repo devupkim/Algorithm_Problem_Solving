d40b1b934b79df1fa7905569713de9b1d035d119
Taehyun Kim <devupkim@gmail.com>
2024-04-02 23:35:13 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,19 @@
+#pragma GCC optimize("O3")
+
+#include <bits/stdc++.h>
+
+#define endl "\n"
+#define ll long long
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    string a;
+    cin >> a;
+    int i;
+    cin >> i;
+    cout << a[i - 1];
+    return 0;
+}
