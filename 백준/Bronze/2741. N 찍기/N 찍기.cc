3a81e2392d5f3832c78cff2f558f353458e9cc58
Taehyun Kim <devupkim@gmail.com>
2024-04-06 23:58:15 +0900

    [Bronze V] Title: N 찍기, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.cc
@@ -0,0 +1,21 @@
+#pragma GCC optimize("O3")
+
+#include <bits/stdc++.h>
+
+#define endl "\n"
+#define ll long long
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+    int i = 1;
+    do {
+        cout << i++ << endl;
+    } while (i <= n);
+
+    return 0;
+}
