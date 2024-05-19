4049c34f198f7da214a774b67672db35781ca2e7
Taehyun Kim <devupkim@gmail.com>
2024-05-19 22:31:24 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.01 ms, Memory: 4.02 MB -BaekjoonHub

---
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string my_string, string overwrite_string, int s) {
+    for(int i=s;i<s+overwrite_string.length();i++){
+        my_string[i]=overwrite_string[i-s];
+    }
+    string answer = my_string;
+    return answer;
+}
