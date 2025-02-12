4193fede785604bee2cbfa9eb73ad2ef2ff66ba9
Taehyun Kim <devupkim@gmail.com>
2025-02-13 04:26:14 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
@@ -6,21 +6,21 @@
 #define ll long long
 using namespace std;
 
+int a[26];
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int a[26] = {0,};
-
     string s;
+
     cin >> s;
-    for (int i = 0; i < s.length(); i++) {
-        a[s[i] - 'a']++;
-    }
 
-    for (int i = 0; i < 26; i++) {
-        cout << a[i] << " ";
+    for (auto i: s) {
+        a[i - int('a')] ++;
     }
-
+    for (auto i: a) {
+        cout << i << " ";
+    }
     return 0;
 }
