e83b2b75d327651efbc784e8a932c4feeb088f6f
Taehyun Kim <devupkim@gmail.com>
2025-09-17 22:21:36 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.cc
@@ -0,0 +1,16 @@
+#pragma GCC optimize("O3")
+
+#include <bits/stdc++.h>
+
+#define endl "\n"
+#define ll long long
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int a, b;
+    cin >> a >> b;
+    cout << a + b;
+}
