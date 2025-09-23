dc13ad16d50998f8c7e5cf2881e7957460736846
Taehyun Kim <devupkim@gmail.com>
2025-09-24 01:15:19 +0900

    [Silver III] Title: 두 수의 합, Time: 8 ms, Memory: 4868 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.cc
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.cc
@@ -5,31 +5,29 @@
 #define endl "\n"
 #define ll long long
 using namespace std;
+bool a[2'000'001] = {false,};
+vector<int> numbers;
 
-const int MAX = 2'000'000;
-int arr[MAX + 1];
-
-int main(void) {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
     int n;
     cin >> n;
-    for (int i = 1; i <= n; i++) {
-        int a;
-        cin >> a;
-        arr[a]++;
+    for (int i = 0; i < n; i++) {
+        int k;
+        cin >> k;
+        a[k] = true;
+        numbers.push_back(k);
     }
 
     int x;
     cin >> x;
 
     int cnt = 0;
-
-    for (int i = 1; i < (x + 1) / 2; i++) {
-        int target = x - i;
-        if (arr[i] && arr[target]) {
-            cnt += arr[i] * arr[target];
+    for (int num : numbers) {
+        if (x - num > 0 && a[x - num]) {
+            if (num < x - num) cnt++;
         }
     }
     cout << cnt;
