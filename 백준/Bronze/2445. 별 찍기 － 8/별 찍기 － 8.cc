8f70b0d2ed1e8cc6125c1ae37eb1ad378fbb335a
Taehyun Kim <devupkim@gmail.com>
2025-09-30 20:13:29 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.cc
@@ -0,0 +1,29 @@
+#pragma GCC optimize("O3")
+
+#include <bits/stdc++.h>
+
+#define endl "\n"
+#define ll long long
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        for (int k = 1; k <= i; k++) cout << "*";
+        for (int k = 2 * (n - i); k > 0; k--) cout << " ";
+        for (int k = 1; k <= i; k++) cout << "*";
+        cout << endl;
+    }
+    for (int i = n - 1; i >= 1; i--) {
+        for (int k = 1; k <= i; k++) cout << "*";
+        for (int k = 2 * (n - i); k > 0; k--) cout << " ";
+        for (int k = 1; k <= i; k++) cout << "*";
+        cout << endl;
+    }
+}
